8cc8d962673b654997a7ccd755284278abe80f08
Luca Desimone <103000008+Chapa007@users.noreply.github.com>
2023-01-06 15:22:22 -0300

    16

--- src/main/java/com/portfolio/arg/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/arg/Security/Controller/AuthController.java
@@ -14,7 +14,6 @@ import java.util.Set;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
