0a79930427d4f9480fa100bc843814bfc36271c1
Luca Desimone <103000008+Chapa007@users.noreply.github.com>
2023-01-05 23:04:14 -0300

    12

--- src/main/java/com/portfolio/arg/Controller/CEducacion.java
+++ src/main/java/com/portfolio/arg/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://argprograma-93cad.web.app")
+@CrossOrigin(origins = "https://mid-virgina-chapa007.koyeb.app")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
