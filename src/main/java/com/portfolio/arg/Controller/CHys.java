e560bd4b35abf7301e7672c997bafbe204e23c2b
Luca Desimone <103000008+Chapa007@users.noreply.github.com>
2023-01-09 20:31:08 -0300

    Fix Url

--- src/main/java/com/portfolio/arg/Controller/CHys.java
+++ src/main/java/com/portfolio/arg/Controller/CHys.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin(origins = "https://mid-virgina-chapa007.koyeb.app/")
+@CrossOrigin(origins = "https://argprograma-93cad.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
