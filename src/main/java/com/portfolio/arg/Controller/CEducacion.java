cb3e51725a9d7c611e26f014fbbd123ae4e483b4
Luca Desimone <103000008+Chapa007@users.noreply.github.com>
2023-01-12 00:27:21 -0300

    DataBase25

--- src/main/java/com/portfolio/arg/Controller/CEducacion.java
+++ src/main/java/com/portfolio/arg/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://argprograma-93cad.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://argprograma-93cad.web.app")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/portfolio/arg/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/arg/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://argprograma-93cad.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://argprograma-93cad.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
